7abd95ebac4200b46b4d09c1edafb6d1c03ab20b
RinatGibadullin <Rinat990104@yandex.ru>
2019-11-17 14:15:57 +0300

    fix star

--- src/Components/RepositoryList.js
+++ src/Components/RepositoryList.js
@@ -6,12 +6,7 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Icon from '@material-ui/core/Icon';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import makeStyles from "@material-ui/core/styles/makeStyles";
-
 
 const RepositoryList = ({
                             repositories,
@@ -46,7 +41,7 @@ const RepositoryList = ({
                                     />
                                 </Button>
                                 <Button size="small" color="primary">
-                                    <Star id={node}/>
+                                    <Star node={node}/>
                                 </Button>
                             </CardActions>
                         </li>
--- src/Components/Star.js
+++ src/Components/Star.js
@@ -3,14 +3,14 @@ import React from "react";
 import {STAR_REPOSITORY} from './App';
 import Icon from '@material-ui/core/Icon';
 
-const Star = (node) => (
-    <Mutation mutation={STAR_REPOSITORY} variables={ node.id }>
+const Star = ({node}) => {
+    const {viewerHasStarred,id} = node;
+    return <Mutation mutation={STAR_REPOSITORY} variables={{id}}>
         {starRepository => (
-            <Icon color={node.viewerHasStarred ? 'secondary':'disabled' } onClick={starRepository}>
+            <Icon color={viewerHasStarred ? 'secondary' : 'disabled'} onClick={starRepository}>
                 star
             </Icon>
         )}
     </Mutation>
-);
-
+};
 export default Star;
