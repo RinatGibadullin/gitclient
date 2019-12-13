8fe0a9fa7c1aafaea844b5f50293409e68df246d
RinatGibadullin <Rinat990104@yandex.ru>
2019-12-13 15:04:56 +0300

    Updates

---
+++ precache-manifest.4a8f15abaa6cfd8b94dc66663153ce11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6917c966308b49803a32da1ab526f945",
+    "url": "/GitClient/index.html"
+  },
+  {
+    "revision": "7ccd3995830b47b38693",
+    "url": "/GitClient/static/css/2.bc3aac6f.chunk.css"
+  },
+  {
+    "revision": "b16ab06c423d686c5f64",
+    "url": "/GitClient/static/css/main.98ea214b.chunk.css"
+  },
+  {
+    "revision": "7ccd3995830b47b38693",
+    "url": "/GitClient/static/js/2.fdd4ac4f.chunk.js"
+  },
+  {
+    "revision": "b16ab06c423d686c5f64",
+    "url": "/GitClient/static/js/main.e9d9d8b4.chunk.js"
+  },
+  {
+    "revision": "706d20ebaaefd39583c9",
+    "url": "/GitClient/static/js/runtime~main.4bbb244c.js"
+  }
+]);
